a72cd5eea06373a9f00c76237f888c16a852b2e2
Anyll <anyllmarkevich@gmail.com>
2020-02-04 19:27:55 -0700

    Started working on creating a system to handle the UI, including a decoder class. This class still doesn't have an instance. Is it possible to create a static class like in Swift and Xcode????

--- UIHandler.py
+++ UIHandler.py
@@ -1,4 +1,18 @@
-def askForCode():
-    return input("Please input code.")
-def askForKey():
-    return input("Please input key.")
+########## DECODE FUNCTIONS ##########
+class Decoder:
+    def askForCode():
+        return input("Please input code.")
+    def askForKey():
+        return input("Please input key.")
+    def isCodeValid(code):
+        if code.isnum():
+            return True
+        else:
+            return False
+    def userDecodeStyle():
+        print('''How do you want to have the code decoded?
+        1 = Each letter's number is associated with the first letter of a word in the key
+        2 = Each letter's number is associated with a charactor in the key''')
+        answer = input()
+        if answer == "1" or answer == "2":
+            return answer
